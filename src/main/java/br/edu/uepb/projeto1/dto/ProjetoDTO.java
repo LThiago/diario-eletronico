91f21b0652f435591f8d9cf0add1dbcf3afd13a8
Luis Thiago <LThiagoCH@gmail.com>
2021-09-03 20:11:16 -0300

    Initial commit

---
+++ src/main/java/br/edu/uepb/projeto1/dto/ProjetoDTO.java
@@ -0,0 +1,17 @@
+package br.edu.uepb.projeto1.dto;
+
+//import java.util.Set;
+
+import lombok.Data;
+
+@Data
+public class ProjetoDTO {
+    
+    private Long id;
+    private String nome;
+    private String sala;
+    private String codigo;
+    /*private Set<AlunoDTO> alunos;
+    private Set<ProfessorDTO> professores;*/
+
+}
