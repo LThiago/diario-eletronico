e57acec39270f6319087e3fc2407d7439859ee7e
Gustavo Silva <ghustavosm@gmail.com>
2021-09-04 02:26:56 -0300

    Atualizações

--- src/main/java/br/edu/uepb/projeto1/controller/ProjetoController.java
+++ src/main/java/br/edu/uepb/projeto1/controller/ProjetoController.java
@@ -1,13 +1,18 @@
 package br.edu.uepb.projeto1.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import br.edu.uepb.projeto1.domain.Aluno;
 import br.edu.uepb.projeto1.domain.Projeto;
 import br.edu.uepb.projeto1.dto.ProjetoDTO;
+import br.edu.uepb.projeto1.mapper.AlunoMapper;
+import br.edu.uepb.projeto1.mapper.ProfessorMapper;
 import br.edu.uepb.projeto1.mapper.ProjetoMapper;
+import br.edu.uepb.projeto1.services.AlunoService;
 import br.edu.uepb.projeto1.services.ProjetoService;
-
+import br.edu.uepb.projeto1.dto.AlunoDTO;
 import br.edu.uepb.projeto1.dto.GenericResponseErrorDTO;
 import br.edu.uepb.projeto1.exceptions.ExistingSameNameException;
 import br.edu.uepb.projeto1.exceptions.NaoEncontradoException;
@@ -33,6 +38,15 @@ public class ProjetoController {
     @Autowired
     private ProjetoMapper projetoMapper;
 
+    @Autowired
+    private ProfessorMapper professorMapper;
+
+    @Autowired
+    private AlunoMapper alunoMapper;
+
+    @Autowired
+    private AlunoService alunoService; 
+
     @GetMapping
     @ApiOperation(value = "Obtém uma lista de projetos")
     public List<ProjetoDTO> getProjetos() {
@@ -47,6 +61,26 @@ public class ProjetoController {
     public ResponseEntity<?> getProjetoById(@PathVariable Long id) {
         try {
             return new ResponseEntity<>(projetoMapper.convertToProjetoDTO(projetoService.findById(id)), HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return ResponseEntity.badRequest().body(new GenericResponseErrorDTO(e.getMessage()));
+        }
+    }
+
+    @Transactional
+    @GetMapping("/{id}/alunos")
+    @ApiOperation(value = "Obtém os alunos de um projeto a partir do ID")
+    public List<AlunoDTO> getAlunosProjeto(@PathVariable Long id) throws NotFoundException {
+        List<Aluno> alunos = new ArrayList<>(projetoService.findById(id).getAlunos());
+        return alunos.stream()
+                        .map(alunoMapper::convertToAlunoDTO)
+                        .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}/coordenador")
+    @ApiOperation(value = "Obtém o coordenador de um projeto a partir do ID")
+    public ResponseEntity<?> getCoordenadorProjeto(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(professorMapper.convertToProfessorDTO(projetoService.findById(id).getCoordenador()), HttpStatus.OK);
         } catch (NotFoundException e) {
             return ResponseEntity.badRequest().body(new GenericResponseErrorDTO(e.getMessage()));
         }
